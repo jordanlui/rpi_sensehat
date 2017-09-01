bf74a9c0d79bb24bde4d2d15b100a700c07be595
jordanlui <jordan.lui@gmail.com>
2017-09-01 05:35:44 +0000

    very first steps

---
+++ aug31.py
@@ -0,0 +1,14 @@
+# SenseHAT RPi Evaluation
+# element14
+# Jordan Lui 2017
+
+print('Element14 Raspberry Pi Evaluation')
+
+# Operating parameters
+
+
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+sense.show_message('Hellow my name is Jordan')
